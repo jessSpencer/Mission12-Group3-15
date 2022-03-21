4f491acea7398435191499eac625517350882abe
Jessie Spencer <jeskspencer@gmail.com>
2022-03-21 14:31:30 -0600

    first change/models made

---
+++ Models/Appointment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission12_Group3_15_.Models
+{
+    public class Appointment
+    {
+    }
+}
